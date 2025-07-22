09391eee0c65072b4235dcb8523ba261ef964db8
VCSurya <suresh.prajapati@vc-erp.com>
2025-07-22 15:29:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    x = input('Enter Number:')
+    print('Input:',x)
     return 'Your first server of Flask is running'
 
 if __name__ == '__main__':
